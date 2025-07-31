f4f0aa9f7531b496415b1b12e79d466b2863ee9c
M Hussnain Nawaz <147604715+MHussnainNawaz@users.noreply.github.com>
2025-08-01 02:54:35 +0500

    third

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 4200;
 
+// Serve static file directly from root
 app.get('/', (req, res) => {
-  res.send('Hello from Node.js on port 4200!');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
