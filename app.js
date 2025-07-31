7e7f6b9f1e64b52faea43779b155639a4dada764
M Hussnain Nawaz <147604715+MHussnainNawaz@users.noreply.github.com>
2025-08-01 02:43:32 +0500

    second

--- app.js
+++ app.js
@@ -1,17 +1,11 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 const port = 4200;
 
-// Serve static files from public/
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Route
-app.get('/api', (req, res) => {
-  res.json({ message: "API is working from EC2!" });
+app.get('/', (req, res) => {
+  res.send('Hello from Node.js on port 4200!');
 });
 
-// Start server
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
